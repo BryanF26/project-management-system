1d0c5ad1297ec4dde591d466dffb147d248be3d7
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-30 10:33:01 +0700

    Update enum.kt

--- project-management-system/src/enum.kt
+++ project-management-system/src/enum.kt
@@ -2,7 +2,6 @@ enum class ProjectStatus {
     Active, Completed, OnHold
 }
 
-<<<<<<< Updated upstream
 enum class TaskPriority {
     Low, Medium, High
 }
@@ -10,9 +9,7 @@ enum class TaskPriority {
 enum class TaskStatus {
     Not_Started, In_Progress, Completed
 }
-enum class Role {
-=======
+
 enum class Roles {
->>>>>>> Stashed changes
     Manager, Developer, Tester
 }
