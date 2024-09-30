f168bc6b3eb45ff4ff2e49685245fe65d86a1c31
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-30 11:19:25 +0700

    fixing function names

--- project-management-system/src/ProjectManager.kt
+++ project-management-system/src/ProjectManager.kt
@@ -1,39 +1,43 @@
+import java.time.LocalDate
+
 class ProjectManager{
 
     private val projects = mutableListOf<Project>()
     private val tasks = mutableListOf<Task>()
     private val users = mutableListOf<User>()
 
-    fun CreateProject(id: Int, name: String, deadline: LocalDate, status: ProjectStatus) {
+    fun createProject(id: Int, name: String, deadline: LocalDate, status: ProjectStatus) {
         val newProject = Project(id, name, deadline, status)
         projects.add(newProject)
         println("Project '${newProject.name}' has been created with ID: ${newProject.id}.")
     }
 
-    fun AddTaskToProject(){
+    fun addTaskToProject(){
 
     }
 
-    fun AssignTask(){
+    fun assignTask(){
 
     }
 
-    fun UpdateTaskStatus(){
+    fun updateTaskStatus(){
 
     }
 
-    fun ProjectStatusTrakcing(){
+    fun projectStatusTracking(){
 
     }
 
-    fun CreateUser(id: Int, name: String, roleInput: String): User {
+    fun createUser(id: Int, name: String, roleInput: String): User {
         val role = try {
-            Roles.valueOf(roleInput)  // This will throw an IllegalArgumentException if roleInput is invalid
+            Roles.valueOf(roleInput)
         } catch (e: IllegalArgumentException) {
             throw IllegalArgumentException("Invalid role: $roleInput. Must be one of ${Roles.values().joinToString()}")
         }
 
         return User(id, name, role)
     }
+
+
 
 }
