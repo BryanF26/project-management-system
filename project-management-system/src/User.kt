5ca9aff10aaa5153ae8ef87891386b5f07346776
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-30 10:51:26 +0700

    user-name-validation

--- project-management-system/src/User.kt
+++ project-management-system/src/User.kt
@@ -2,4 +2,8 @@ class User(
     var id: Int,
     var name: String,
     var role: Roles
-)
+) {
+    init {
+        if (name.isBlank()) throw IllegalArgumentException("Name cannot be blank")
+    }
+}
