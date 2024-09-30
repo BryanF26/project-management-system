783eb17c003176ce0c2cb752b8d9c755e0ef8d6f
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-30 10:39:20 +0700

    Update ProjectManager.kt

---
+++ project-management-system/src/ProjectManager.kt
@@ -0,0 +1,26 @@
+class ProjectManager{
+
+    private val projects = mutableListOf<Project>()
+    private val tasks = mutableListOf<Task>()
+    private val users = mutableListOf<User>()
+    
+    fun CreateProject(){
+
+    }
+
+    fun AddTaskToProject(){
+
+    }
+
+    fun AssignTask(){
+
+    }
+
+    fun UpdateTaskStatus(){
+
+    }
+
+    fun ProjectStatusTrakcing(){
+
+    }
+}
