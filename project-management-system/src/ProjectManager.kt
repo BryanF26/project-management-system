ff291febd7914206b4ace6eb10441b3d220dbc63
KGeraldy <kennygeraldy@gmail.com>
2024-09-30 11:39:03 +0700

    Project

--- project-management-system/src/ProjectManager.kt
+++ project-management-system/src/ProjectManager.kt
@@ -12,8 +12,19 @@ class ProjectManager{
         println("Project '${newProject.name}' has been created with ID: ${newProject.id}.")
     }
 
-    fun addTaskToProject(){
-
+    fun AddTaskToProject(projectId: Int, taskIds: Array<Int>){
+        val project = projects.find {it.id == projectId}
+        if(project != null) {
+            for (taskId in taskIds) {
+                val task = tasks.find { it.id == taskId }
+                if (task != null) {
+                    project.tasks.add(task)
+                    println("Task with ID: '${taskId}' has been successfully added to Project ID: ${projectId}.")
+                } else {
+                    println("Failed to add Task with ID: '${taskId}' to Project ID: ${projectId}.")
+                }
+            }
+        }
     }
 
     fun assignTask(){
