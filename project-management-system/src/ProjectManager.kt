01c35eb7d0fd0102ece971592fec4f4852d251aa
KGeraldy <kennygeraldy@gmail.com>
2024-09-30 10:56:17 +0700

    add project

--- project-management-system/src/ProjectManager.kt
+++ project-management-system/src/ProjectManager.kt
@@ -3,9 +3,11 @@ class ProjectManager{
     private val projects = mutableListOf<Project>()
     private val tasks = mutableListOf<Task>()
     private val users = mutableListOf<User>()
-    
-    fun CreateProject(){
 
+    fun CreateProject(id: Int, name: String, deadline: LocalDate, status: ProjectStatus) {
+        val newProject = Project(id, name, deadline, status)
+        projects.add(newProject)
+        println("Project '${newProject.name}' has been created with ID: ${newProject.id}.")
     }
 
     fun AddTaskToProject(){
